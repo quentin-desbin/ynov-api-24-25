c01030a988a166aa3df9b66f86946466cfbe0c49
Quentin DESBIN <quentin.desbin@ynov.com>
2024-11-12 10:15:46 +0100

    ✨ add open-api middleware for validate requests

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import usersRouter from './routers/users.router.js'
+import * as OpenApiValidator from 'express-openapi-validator'
+
+// Import path module to get the current directory (node < 20.11.X)
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 // Initialize express
 const app = express()
@@ -8,9 +13,21 @@ const app = express()
 // Use default middlewares
 app.use(cors()) // Cors is a middleware that allows/disallows access to the API
 app.use(express.json()) // Parse incoming requests with JSON payloads
+app.use(OpenApiValidator.middleware({
+    apiSpec: __dirname + '/openapi-main.yaml',
+    ignoreUndocumented: true
+}))
 
 // API routers
 app.use('/users', usersRouter) // When a request is made to /users, the usersRouter will handle it
+
+app.use((err, req, res, next) => {
+    // format error
+    res.status(err.status || 500).json({
+        message: err.message,
+        errors: err.errors,
+    })
+})
 
 // Export the app to be used in the server
 export default app
